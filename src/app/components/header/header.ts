322e4960857d1a63d9b3a3f799e509b4ecb4863b
blessing_u <ubiomorblessing@gmail.com>
2025-11-07 13:46:49 +0100

    feat: implement a reusable button component.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,14 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { PrimaryButton } from '../primary-button/primary-button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButton],
   template: `
-    <div class="bg-slate-100 px-4 py-3 shadow-md">
-      <span>My Store</span>
+    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
+      <span class="text-2xl">My Store</span>
+      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()" />
     </div>
   `,
   styles: `
   `,
 })
-export class Header {}
+export class Header {
+  showButtonClicked() {
+    console.log('clicked');
+  }
+}
